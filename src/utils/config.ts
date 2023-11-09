b089aeae61db8007171cb4baef92d208f6f5ac15
Anbraten <anton@ju60.de>
2023-11-10 00:57:09 +0100

    remove some unused options

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,18 +5,17 @@ import { promises as fs } from "fs";
 const ciConfig = {
   configFile: process.env.PLUGIN_CONFIG_FILE,
   isCI: process.env.CI === "woodpecker",
-  eventType: process.env.PLUGIN_EVENT_TYPE || process.env.CI_PIPELINE_EVENT,
+  eventType: process.env.CI_PIPELINE_EVENT,
   releaseBranch:
     process.env.PLUGIN_RELEASE_BRANCH ||
     process.env.CI_REPO_DEFAULT_BRANCH ||
     "main",
-  commitMessage:
-    process.env.PLUGIN_COMMIT_MESSAGE || process.env.CI_COMMIT_MESSAGE,
-  forgeType: process.env.PLUGIN_FORGE_TYPE || process.env.CI_FORGE_TYPE,
+  commitMessage: process.env.CI_COMMIT_MESSAGE,
+  forgeType: process.env.CI_FORGE_TYPE,
   githubToken: process.env.PLUGIN_GITHUB_TOKEN,
   gitEmail: process.env.PLUGIN_GIT_EMAIL,
-  repoOwner: process.env.PLUGIN_REPO_OWNER || process.env.CI_REPO_OWNER,
-  repoName: process.env.PLUGIN_REPO_NAME || process.env.CI_REPO_NAME,
+  repoOwner: process.env.CI_REPO_OWNER,
+  repoName: process.env.CI_REPO_NAME,
   releasePrefix: "🎉 Release",
   debug: process.env.PLUGIN_DEBUG === "true",
 };
