9414db91b6b7f5d955769aa9de4b39fadbee3f90
6543 <6543@obermui.de>
2024-11-28 16:35:50 +0100

    add actual tests

--- src/utils/change.test.ts
+++ src/utils/change.test.ts
@@ -86,103 +86,130 @@ describe('change', () => {
     vi.setSystemTime(date);
   });
 
-  it('should get the correct major bump', () => {
-    const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithMajor, false);
-    expect(nextVersion).toBe('2.0.0');
+  describe('version bumping', () => {
+    it('should get the correct major bump', () => {
+      const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithMajor, false);
+      expect(nextVersion).toBe('2.0.0');
+    });
+
+    it('should get the correct minor bump', () => {
+      const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithMinor, false);
+      expect(nextVersion).toBe('1.1.0');
+    });
+
+    it('should get the correct patch bump', () => {
+      const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithPatch, false);
+      expect(nextVersion).toBe('1.0.1');
+    });
+
+    it('should get the correct rc bump', () => {
+      const nextVersion = getNextVersionFromLabels('1.2.3', config.user, changesWithMajor, true);
+      expect(nextVersion).toBe('2.0.0-rc.0');
+    });
+
+    it('should get the correct bump from a rc', () => {
+      const nextVersion = getNextVersionFromLabels('1.0.0-rc.2', config.user, changesWithPatch, false);
+      expect(nextVersion).toBe('1.0.0');
+    });
+
+    it('should get the correct rc bump from a previous rc', () => {
+      const nextVersion = getNextVersionFromLabels('1.0.1-rc.2', config.user, changesWithMinor, true);
+      expect(nextVersion).toBe('1.1.0-rc.0');
+    });
   });
 
-  it('should get the correct minor bump', () => {
-    const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithMinor, false);
+  describe('changelog generation', () => {
+    it('should generate a changelog section', () => {
+      const forge = new GithubForge('', '');
+      const changelog = getChangeLogSection('1.0.0', 'v1.0.0', config, changes, forge, true);
+      expect(changelog).toMatchSnapshot();
+    });
 
-    expect(nextVersion).toBe('1.1.0');
-  });
+    describe('changelog text management', () => {
+      const forge = new GithubForge('', '');
+      const newSection = getChangeLogSection('1.0.0', 'v1.0.0', config, changes, forge, true);
 
-  it('should get the correct patch bump', () => {
-    const nextVersion = getNextVersionFromLabels('1.0.0', config.user, changesWithPatch, false);
+      it('should handle empty/no changelog', () => {
+        const oldChangelog = '';
+        const changelog = updateChangelogSection('0.0.0', '1.0.0', oldChangelog, newSection);
+        expect(changelog).toBe(`# Changelog\n\n${newSection}\n`);
+      });
 
-    expect(nextVersion).toBe('1.0.1');
-  });
+      it('should handle changelog without pretext', () => {
+        const oldChangelog = `# Changelog
 
-  it('should get the correct rc bump', () => {
-    const nextVersion = getNextVersionFromLabels('1.2.3', config.user, changesWithMajor, true);
+## [0.1.0](https://example.com/releases/tag/0.1.0) - 2024-01-01
 
-    expect(nextVersion).toBe('2.0.0-rc.0');
-  });
+### Features
+- Initial release\n`;
 
-  it('should get the correct bump from a rc', () => {
-    const nextVersion = getNextVersionFromLabels('1.0.0-rc.2', config.user, changesWithPatch, false);
+        const changelog = updateChangelogSection('0.1.0', '1.0.0', oldChangelog, newSection);
+        expect(changelog).toBe(`# Changelog\n\n${newSection}
 
-    expect(nextVersion).toBe('1.0.0');
-  });
+## [0.1.0](https://example.com/releases/tag/0.1.0) - 2024-01-01
 
-  it('should get the correct rc bump from a previous rc', () => {
-    const nextVersion = getNextVersionFromLabels('1.0.1-rc.2', config.user, changesWithMinor, true);
+### Features
+- Initial release\n`);
+      });
+    });
 
-    expect(nextVersion).toBe('1.1.0-rc.0');
-  });
-
-  it('should generate a changelog', () => {
-    const forge = new GithubForge('', '');
-    const changelog = getChangeLogSection('1.0.0', 'v1.0.0', config, changes, forge, true);
-
-    expect(changelog).toMatchSnapshot();
-  });
-
-  const changelogFiles = [
-    {
-      name: 'should add new section',
-      file: '__fixtures__/CHANGELOG_1.md',
-      latestVersion: '1.0.0',
-      nextVersion: '1.0.1',
-    },
-    {
-      name: 'should update existing changelog section',
-      file: '__fixtures__/CHANGELOG_2.md',
-      latestVersion: '2.0.1',
-      nextVersion: '2.0.3',
-    },
-    {
-      name: 'should remove versions newer than the latest released version',
-      // this happens if we bump the version in an existing release PR
-      file: '__fixtures__/CHANGELOG_3.md',
-      latestVersion: '2.0.1',
-      nextVersion: '2.0.4',
-    },
-    {
-      name: 'should remove previous prerelease versions as soon as the full release is added',
-      file: '__fixtures__/CHANGELOG_4.md',
-      latestVersion: '2.0.1-pre.1',
-      nextVersion: '2.0.1',
-    },
-  ];
-  it.each(changelogFiles)('$name', async ({ file, nextVersion, latestVersion }) => {
-    const oldChangelog = await fs.readFile(path.join(__dirname, file), 'utf8');
-
-    const forge = new GithubForge('', '');
-    const newSection = getChangeLogSection(nextVersion, nextVersion, config, changes, forge, true);
-    const changelog = updateChangelogSection(latestVersion, nextVersion, oldChangelog, newSection);
-
-    expect(changelog).toMatchSnapshot();
-  });
-
-  it('should be able to extract the release version from a commit message', () => {
-    const tests = [
+    const changelogFiles = [
       {
-        commitMessage: '🎉 Release 1.2.3',
-        expectedVersion: '1.2.3',
+        name: 'should add new section',
+        file: '__fixtures__/CHANGELOG_1.md',
+        latestVersion: '1.0.0',
+        nextVersion: '1.0.1',
       },
       {
-        commitMessage: '🎉 Release 1.2.3-rc.0 [skip ci]',
-        expectedVersion: '1.2.3-rc.0',
+        name: 'should update existing changelog section',
+        file: '__fixtures__/CHANGELOG_2.md',
+        latestVersion: '2.0.1',
+        nextVersion: '2.0.3',
       },
       {
-        commitMessage: 'chore(release): 1.0.0-rc.0 [skip ci]',
-        expectedVersion: '1.0.0-rc.0',
+        name: 'should remove versions newer than the latest released version',
+        file: '__fixtures__/CHANGELOG_3.md',
+        latestVersion: '2.0.1',
+        nextVersion: '2.0.4',
+      },
+      {
+        name: 'should remove previous prerelease versions as soon as the full release is added',
+        file: '__fixtures__/CHANGELOG_4.md',
+        latestVersion: '2.0.1-pre.1',
+        nextVersion: '2.0.1',
       },
     ];
+    it.each(changelogFiles)('$name', async ({ file, nextVersion, latestVersion }) => {
+      const oldChangelog = await fs.readFile(path.join(__dirname, file), 'utf8');
 
-    tests.forEach(({ commitMessage, expectedVersion }) => {
-      expect(extractVersionFromCommitMessage(commitMessage)).toBe(expectedVersion);
+      const forge = new GithubForge('', '');
+      const newSection = getChangeLogSection(nextVersion, nextVersion, config, changes, forge, true);
+      const changelog = updateChangelogSection(latestVersion, nextVersion, oldChangelog, newSection);
+
+      expect(changelog).toMatchSnapshot();
+    });
+  });
+
+  describe('version extraction', () => {
+    it('should extract version from various commit messages', () => {
+      const tests = [
+        {
+          commitMessage: '🎉 Release 1.2.3',
+          expectedVersion: '1.2.3',
+        },
+        {
+          commitMessage: '🎉 Release 1.2.3-rc.0 [skip ci]',
+          expectedVersion: '1.2.3-rc.0',
+        },
+        {
+          commitMessage: 'chore(release): 1.0.0-rc.0 [skip ci]',
+          expectedVersion: '1.0.0-rc.0',
+        },
+      ];
+
+      tests.forEach(({ commitMessage, expectedVersion }) => {
+        expect(extractVersionFromCommitMessage(commitMessage)).toBe(expectedVersion);
+      });
     });
   });
 });
